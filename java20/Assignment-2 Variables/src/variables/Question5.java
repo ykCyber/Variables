90c56c99292d17a27b8b2351d290c4be4bf9a503
YusufKenan <59359546+ykCyber@users.noreply.github.com>
2020-09-22 19:26:46 +0200

    Update Question5.java

--- java20/Assignment-2 Variables/src/variables/Question5.java
+++ java20/Assignment-2 Variables/src/variables/Question5.java
@@ -5,6 +5,7 @@ public class Question5 {
 	public static void main(String[] args) {
 
 		String first = ("first is 25");
+		// why do u use paranthessis?
 		String second = (" second = 6776");
 		
 		System.out.println(first + second);
